531a11da0880858e029847e6115568a378818bf0
Colin Bertin <colinbertin12@gmail.com>
2022-02-16 20:36:40 +0900

    refacto

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 puts "Cleaning the db..."
 Restaurant.destroy_all
 
-puts "Creating 10 restaurants..."
-10.times do
+puts "Creating 30 restaurants..."
+30.times do
   Restaurant.create!(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
